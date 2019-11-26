40da24741eaa9481c0afc4c3d7dfafcd07c2bac1
jungrama <junkrama2@gmail.com>
2019-11-27 07:57:09 +0800

    add document ready

--- js/pages/matches.js
+++ js/pages/matches.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
 // NOTE : ALL API SETTING ON => setting.js
 
 /* --------------------- CALL AND INIT DATA ON COMPONENT -------------------- */
